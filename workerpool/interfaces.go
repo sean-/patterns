b54d30b68cb2028d0ea7bb0523ba105ac97330a9
Sean Chittenden <sean@chittenden.org>
2018-08-30 11:33:18 -0700

    Move `Interface` into `interface.go`

--- workerpool/interfaces.go
+++ workerpool/interfaces.go
@@ -2,6 +2,23 @@ package workerpool
 
 import "context"
 
+// Interface is the workerpool interface that is exported
+type Interface interface {
+	// ShutdownCtx is the shutdown context, used to poll for the status of shutdown
+	ShutdownCtx() context.Context
+
+	// InitiateShutdown cancels the context used to signal its time to shut down
+	InitiateShutdown() (bool, error)
+
+	// Reload is a configuration reload handler (e.g. for use with SIGHUP)
+	Reload()
+
+	StartProducers() error
+	StartConsumers() error
+	WaitProducers() error
+	WaitConsumers() error
+}
+
 // Factories is a container for different types of factories.
 type Factories struct {
 	ProducerFactory ProducerFactory
